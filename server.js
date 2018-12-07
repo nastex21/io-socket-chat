d34bc6179eb45327a6e95d86f29c5892941fc5f3
Tony <nasax2000@gmail.com>
2018-12-07 00:11:35 -0600

    Commit

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
-const express     = require('express');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const auth        = require('./app/auth.js');
-const routes      = require('./app/routes.js');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-const cookieParser= require('cookie-parser')
-const app         = express();
-const http        = require('http').Server(app);
-const sessionStore= new session.MemoryStore();
-const io = require('socket.io')(http)
-const cors = require('cors')
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const auth = require('./app/auth.js');
+const routes = require('./app/routes.js');
+const mongo = require('mongodb').MongoClient;
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const app = express();
+const http = require('http').Server(app);
+const sessionStore = new session.MemoryStore();
+const io = require('socket.io')(http);
+const cors = require('cors');
+const passportSocketIo = require("passport.socketio");
 
 app.use(cors())
 
@@ -21,7 +22,9 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('view engine', 'pug')
 
 app.use(session({
@@ -33,29 +36,38 @@ app.use(session({
 }));
 
 mongo.connect(process.env.MONGO_URI, (err, client) => {
-    var db = client.db('user-db');
-    if(err) console.log('Database error: ' + err);
-  
-    auth(app, db);
-    routes(app, db);
-  
-    http.listen(process.env.PORT || 3000);
+  var db = client.db('user-db');
+  if (err) console.log('Database error: ' + err);
 
-    //start socket.io code  
-    var currentUsers = 0;  
-    io.on('connection', socket => {
+  auth(app, db);
+  routes(app, db);
 
-      console.log('A user has connected');
-      
-      ++currentUsers;
+  http.listen(process.env.PORT || 3000);
 
-      io.emit('user count', currentUsers);
+  //start socket.io code  
+  var currentUsers = 0;
 
-      socket.on('disconnect', () => { --currentUsers; 
-        console.log("A user has disconnected.")});
-})
+  io.use(passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore
+  }));
+
+  io.on('connection', socket => {
+
+    console.log('A user has connected');
+    console.log('user ' + socket.request.user.name + ' connected');
+    ++currentUsers;
+
+    io.emit('user count', currentUsers);
+
+    socket.on('disconnect', () => {
+      --currentUsers;
+      console.log("A user has disconnected.")
+    });
   })
 
-    //end socket.io code
-  
+  //end socket.io code
+
 });
